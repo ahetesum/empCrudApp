9db14dd9224ac86f13395d810bad3f62a5ccab69
hb <hb@hbs-MacBook-Pro-3.local>
2023-05-16 15:59:21 +0530

    edit update

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -43,3 +43,40 @@ exports.getById = (req,res,next)=>{
         });
     })
 }
+
+
+exports.update = (req,res,next)=>{
+    if(ObjectId.isValid(req.params.id)==false)
+    {
+        res.status(400).json({
+            error:"given object id is not valid"
+        })
+    }
+    else
+    employeeCrud.findByIdAndUpdate(req.params.id,req.body)
+    .then((data)=>{
+        res.status(200).json(data);
+    }).catch(err=>{
+        res.status(400).json({
+            error: err
+        });
+    })
+}
+
+exports.delete = (req,res,next)=>{
+    if(ObjectId.isValid(req.params.id)==false)
+    {
+        res.status(400).json({
+            error:"given object id is not valid"
+        })
+    }
+    else
+    employeeCrud.findByIdAndRemove(req.params.id)
+    .then(()=>{
+        res.status(200).json({"status":"deleted Sucessfully"});
+    }).catch(err=>{
+        res.status(400).json({
+            error: err
+        });
+    })
+}
--- src/services/index.js
+++ src/services/index.js
@@ -3,5 +3,8 @@ exports.generateCrudMethods= Model=>{
         getAll:()=>Model.find(),
         getById:id=>Model.findById(id),
         create:record=>Model.create(record),
+        findByIdAndUpdate:(id,record)=>Model.findByIdAndUpdate(id,record),
+        findByIdAndRemove:id=>Model.findByIdAndRemove(id),
+
     }
 }
